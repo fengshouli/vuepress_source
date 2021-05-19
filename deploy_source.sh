1ececa866fd429c0b6d7817d5128369ac2ae51bb
fengshouli <435876110@qq.com>
2021-05-19 22:51:53 +0800

    将源码发送到git

--- deploy_source.sh
+++ deploy_source.sh
@@ -1,6 +1,6 @@
 
 #git init
-git add deploy.sh docs/* package.json deploy_source.sh .travis.yml
+git add deploy.sh docs/* package.json deploy_source.sh
 git commit -m '将源码发送到git'
 
 git push -f git@github.com:fengshouli/vuepress_source.git master
