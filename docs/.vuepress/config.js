0e50216fa10331453ae8f0f4968ccdc4fb38cf49
fengxinyang <fengxyp@yonyou.com>
2021-06-07 19:18:34 +0800

    将源码发送到git

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,16 +17,18 @@ module.exports = {
             { text: 'java知识', link: '/java/' },
             { text: '数据库知识', link: '/数据库/' },
             { text: '源码解析', link: '/源码解析/' },
+            { text: '程序员英语', link: '/程序员英语/' },
             { text: '算法体系', link: '/算法体系/' }
             
         ],
         sidebar: {
-            "/java/":["","java_base","lambda","juc","Kafka","设计模式","Redis","spring","SpringBoot","SpringCloud","总结的题目"],
+            "/java/":["","代码规范","java_base","lambda","juc","Kafka","设计模式","Redis","spring","SpringBoot","SpringCloud","总结的题目"],
             "/数据库/":["","mysql"],
             "/操作/":["","Git","github-CICD","npm","Maven","sql","other","软件插件安装"],
             "/工作经验/":["","实用的类","problem","experience","知识积累"],
             "/项目经验/":["","服务拆分-接口设计","微服务项目架构"],
-            "/源码解析/":["","springboot源码解析.md"],
+            "/源码解析/":["","springboot源码解析"],
+            "/程序员英语/":["","程序员英语"],
             "/算法体系/":["","算法基础.md"]
         }
     }
