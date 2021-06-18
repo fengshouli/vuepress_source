963861bf5bb3360754e06c3650baf6bafb11e0ca
fengshouli <435876110@qq.com>
2021-06-18 23:34:48 +0800

    将源码发送到git

--- deploy_source.sh
+++ deploy_source.sh
@@ -1,6 +1,6 @@
 
 #git init
-git add deploy.sh docs/* package.json package-lock.json deploy_source.sh .github node-1.0-SNAPSHOT-jar-with-dependencies.jar CNAME .gitignore
+git add .
 git commit -am '将源码发送到git'
 
 git push -f git@github.com:fengshouli/vuepress_source.git master
