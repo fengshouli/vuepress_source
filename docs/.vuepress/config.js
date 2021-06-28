913a01891aa3e6787ca4584494c931ae07dda74d
fengxinyang <fengxyp@yonyou.com>
2021-06-28 09:52:34 +0800

    将源码发送到git

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,7 @@ module.exports = {
         ],
         sidebar: {
             "/java/":["","代码规范","java_base","execption","lambda","juc","Kafka","设计模式","Redis","spring","SpringBoot","SpringCloud","总结的题目"],
-            "/数据库/":["","mysql"],
+            "/数据库/":["","数据库操作","mysql"],
             "/操作/":["","Git","github-CICD","npm","Maven","sql","other","软件插件安装"],
             "/工作经验/":["","实用的类","problem","experience","知识积累"],
             "/项目经验/":["","服务拆分-接口设计","微服务项目架构"],
