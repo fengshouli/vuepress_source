0c9fa1e7d5c3c85e69d6245c19bac53b280e4bf3
fengshouli <435876110@qq.com>
2021-10-30 00:07:28 +0800

    将源码发送到git

--- deploy_source.sh
+++ deploy_source.sh
@@ -3,6 +3,6 @@
 git add .
 git commit -am '将源码发送到git'
 
-git push -f git@github.com:fengshouli/vuepress_source.git master
-
+# git push -f git@github.com:fengshouli/vuepress_source.git master
+git push origin master
 cd -
