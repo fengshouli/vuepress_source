48b21ebc5808f31591346a1b1e2244d85ba6d8e2
fengshouli <435876110@qq.com>
2022-01-18 15:28:07 +0800

    将源码发送到git

--- deploy_source.sh
+++ deploy_source.sh
@@ -1,8 +1,12 @@
 
 #git init
+echo "开始.."
 git add .
+echo "添加所有文件,准备提交"
 git commit -am '将源码发送到git'
-
+echo "提交成功,准备push."
+sleep 1s
 # git push -f git@github.com:fengshouli/vuepress_source.git master
 git push origin master
+echo "完成"
 cd -
